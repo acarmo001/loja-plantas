de7b1361c886db54ef5d31114f81af72db61aa2d
Ana Carmo <anaccarmo@hotmail.com>
2024-06-08 21:21:58 +0100

    Página Home - estilização da secção - Categorias

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -20,12 +20,12 @@ export default function Home(){
 
     return (
         <main>
-            <section className="first-section">
+            <section className="bestsellers-section">
                 <div>
                     <h2>As mais vendidas</h2>
                     <p>A maneira mais fácil de ter uma vida saudável é comprar suas plantas favoritas</p>
-                    <div className="rectangulo">
-                        <NavLink to="/products">ver mais<img src="/images/ArrowRight.png" alt="" /></NavLink>
+                    <div>
+                        <button type="button"><NavLink to="/products">ver mais<img src="/images/ArrowRight.png" alt="" /></NavLink></button> 
                     </div>              
                     
                 </div>                             
@@ -47,50 +47,52 @@ export default function Home(){
                 </div>
             </section>
 
-            <section className="second-section">
+            <section className="aboutus-section">
                 <h2>Quem somos</h2>
                 <p>Encomende agora e aprecie a beleza da natureza</p>
-                <div className="horizdisplay">                
+                <div className="aboutus-display">                
                     <div>
                         <img src="/images/Group 1V1.png" alt="" />
-                        <h3>Grande variedade</h3>
+                        <h3><strong>Grande variedade</strong></h3>
                         <p>oferecemos diferentes tipos de produtos com menos variações de cada categoria</p>
                     </div>
                     <div>
                         <img src="/images/Group 1v2.png" alt="" />
-                        <h3>Envio rápido e gratuito</h3>
+                        <h3><strong>Envio rápido e gratuito</strong></h3>
                         <p>Prazo de entrega de 4 dias ou menos, envio grátis e opção de entrega rápida</p>
                     </div>
                     <div>
                         <img src="/images/Group 1v3.png" alt="" />
-                        <h3>Suporte 24/7</h3>
+                        <h3><strong>Suporte 24/7</strong></h3>
                         <p>respostas a qualquer consulta relacionada a negócios 24 horas por dia, 7 dias por semana e em tempo real</p>
                     </div> 
                 </div>
             </section>
 
-            <section className="third-section">
+            <section className="category-section">
                 <h2>Categorias</h2>
-                <p></p>
-                <ul>
-                    <li>
-                        <img src="/images/pexels-julia-kuzenkov-442028-1974508.jpg" alt="" />
-                        <p><strong>Plantas Exteriores</strong></p>                        
-                    </li>
-                    <li>
-                        <img src="/images/pexels-shottrotter-2249959.jpg" alt="" />
-                        <p><strong>Acessórios</strong></p>                        
-                    </li>
-                    <li>
+                <p>Encontre o que procura</p>
+                <div className="category-display">
+                    <div className="imgUp">
+                        <img src="/images/pexels-vlada-karpovich-7902930.jpg" alt="" />
+                        <h3>Plantas Exteriores</h3>                        
+                    </div>
+                    <div className="imgDown">
                         <img src="/images/pexels-kate-amos-1408770-2718447.jpg" alt="" />
-                        <p><strong>Plantas Interiores</strong></p>                        
-                    </li>
-                    <button type="button">Explorar</button>
-                </ul>
+                        <h3>Acessórios</h3>                        
+                    </div>
+                    <div className="imgUp">
+                        <img src="/images/pexels-valeriya-827518.jpg" alt="" />
+                        <h3>Plantas Interiores</h3>                        
+                    </div>                    
+                </div>
+                <div>
+                    <button type="button"><NavLink to="/products">Explorar<img src="/images/ArrowRight.png" alt="" /></NavLink></button> 
+                </div>             
                 
             </section>
 
-            <section className="fourth-section">
+            <section className="Comments-section">
                 <p>O que dizem os nossos clientes sobre "Plantas aos Molhos"</p>
                 <ul>
                     <li>
